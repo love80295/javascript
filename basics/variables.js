7cd8e64a9e4f249958ffcf1c0d141259f90bda2a
loveagrawal <loveagrawal80295@gmail.com>
2026-05-22 16:08:26 +0530

    variable notes

---
+++ basics/variables.js
@@ -0,0 +1,28 @@
+const account = "12345";
+let b = 1234;
+var c = '2345';
+// console.log(account);
+// console.log(b);
+// console.log(c);
+console.table([account , b , c]);
+"use strict" // will be treated as a newer version of js
+//alert(3+2) we are using node js not browser , this works for node js as well but in different manner 
+
+
+                 //  datatypes 
+                 
+// 1 - > numer
+// 2 - > string
+// 3 -- > bigint
+// 4 -> boolean
+// 5 - >  null - > satnd alone value
+// example - > let number = null;
+// 6 -- > undefine - > when the value is not define , variable is defined
+// example let number;
+// 7 - > symbol -- > use to find the uniqueness
+      // these all are premetive datatypes
+
+
+console.log(typeof account); // typeoff is used to find the type of specific variable 
+console.log(typeof null) ; // typeoff null is object becuse it is historical bug but in reality null is not a object
+     
