961ef31a6b2522e091baae1b1f047508aeb5a747
loveagrawal <loveagrawal80295@gmail.com>
2026-05-27 15:27:33 +0530

    switch

---
+++ control_flow/Switch.js
@@ -0,0 +1,24 @@
+ // switch case
+
+ const month = 4;
+ switch (month) {
+        case 1:
+            console.log("jan");
+            break;
+
+        case 2:
+            console.log("feb");
+            break;
+        case 3:
+            console.log("mar");
+            break;
+        case 4:
+            console.log("april");
+            break;
+        case 5:
+            console.log("may");
+            break;    
+        default:
+            console.log("galat he")
+            break;
+ }
