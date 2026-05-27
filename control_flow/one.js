50efa352837996c309ad243115c51be710961e90
loveagrawal <loveagrawal80295@gmail.com>
2026-05-27 15:20:04 +0530

    if else notes

---
+++ control_flow/one.js
@@ -0,0 +1,21 @@
+ // if
+
+ if(2==='2'){     // === also check data type
+     // condition true this will run
+     console.log("chal raha h bsdk")
+ }else{
+    // else this will run
+    console.log("nahi chal raha")
+ }
+
+
+
+  if(2==='2'){     // === also check data type
+     // condition true this will run
+     console.log("chal raha h bsdk")
+ }else if(condition2){
+    // else this will run
+    console.log("nahi chal raha")
+ }else{
+    // here something will else run
+ }
