495a5af2101e7d1e9d2932ce0c67de94c4559163
loveagrawal <loveagrawal80295@gmail.com>
2026-05-27 16:05:54 +0530

    for loop notes

---
+++ Loops/ForLoop.js
@@ -0,0 +1,13 @@
+ // for loop //
+
+ for(let i = 1 ; i<11 ; i++){
+    console.log(i);
+ }
+
+ for (let i = 1 ; i<10 ; i++){
+    //if(i==5) break; // break
+    if(i==5) continue;
+    console.log(i);
+ }
+
+ 
